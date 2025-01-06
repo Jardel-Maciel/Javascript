0b2347ec1aff8daea4d4b85862970b0a7a812ff5
JARDEL MACIEL  DOS SANTOS <jardel.maciel22@gmail.com>
2025-01-06 15:18:25 -0300

    ex 15

---
+++ Aula15ex/modelo/script.js
@@ -0,0 +1,13 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0){
+        window.alert('[ERRO] Por favor dite um numero ')
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        while(c <= 10){
+            let item = document.createElement('option')
+        }
+    }
+}

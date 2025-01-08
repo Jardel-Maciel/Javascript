cd0391491e1dfac6a77639dbc5c74b2bedbfbe38
JARDEL MACIEL  DOS SANTOS <jardel.maciel22@gmail.com>
2025-01-08 20:40:46 -0300

    aula 16

---
+++ Aula16/anbiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+num[5] = 10
+num.push(15)
+
+console.log(`nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+ for (let pos = 0;pos<num.length; pos++)
+    console.log(pos)
---
+++ Aula16/vetornatela.js
@@ -0,0 +1,9 @@
+let valores = [8,2,5,3,6,8,1,0]
+/*for (let pos=0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+}
+*/
+for(let pos in valores){
+    console.log(`A posição ${pos} Tem o valor ${valores[pos]}`)
+}
